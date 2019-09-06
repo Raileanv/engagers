99cc35bf638aafee12cb2b005267a37a19bb67b7
Raileanv <raileanv@gmail.com>
2019-09-06 15:34:31 +0300

    presentations by user 3 for vasia

--- main.go
+++ main.go
@@ -159,7 +159,7 @@ func main() {
 				models.GetPresentationSessionsHandler(w, r, params)
 			})
 			rr.Get("/", models.GetPresentationsHandler)
-			rr.Get("/for_vasia", models.GetPresentationsForVasiaHandler)
+			rr.Get("/for_vasia", models.GetPresentationsPerUserHandler)
 		})
 
 		r.Group("/conference", func(rr martini.Router) {
