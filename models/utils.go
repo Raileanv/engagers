942a68df5f2d44f191979fa8d1c52c002e32b381
Raileanv <raileanv@gmail.com>
2019-09-06 10:21:30 +0300

    try parse time

--- models/utils.go
+++ models/utils.go
@@ -7,10 +7,10 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"net/url"
 	"time"
-	"os"
+	//"os"
 )
 
-//const connStr = "user=vrailean password=5b34b4ccc dbname=prezentr_admin_development host=localhost sslmode=disable"
+const connStr = "user=vrailean password=5b34b4ccc dbname=prezentr_admin_development host=localhost sslmode=disable"
 
 //os.Getenv("DATABASE_URL")
 var (
@@ -29,7 +29,7 @@ func IsCurrentUserPresent() (b bool) {
 
 func InitDB() *gorm.DB {
 	fmt.Println(DB)
-	DB, _ = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	DB, _ = gorm.Open("postgres", connStr)
 	if DB == nil {
 		panic("db nil")
 	}
