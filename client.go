859930c7ab93569857c1a511e4993cd88274c7ff
Raileanv <raileanv@gmail.com>
2019-09-06 13:19:55 +0300

    fix

--- client.go
+++ client.go
@@ -122,10 +122,11 @@ func processMessage(m *Message) *Message {
 		data, ok := m.Data.(map[string]interface{})
 
 		id, _ := strconv.ParseInt(data["quiz_id"].(string), 10, 32)
+		answer_id, _ := strconv.ParseInt(data["answer_id"].(string), 10, 32)
 
 		quizAnswer := models.QuizAnswer{SessionID: uint(m.Client.sessionId), QuizID: uint(id)}
 		if ok {
-			quizAnswer.AnswerID = uint(data["answer_id"].(float64))
+			quizAnswer.AnswerID = uint(answer_id)
 			quizAnswer.UserID = models.CurrentUser.ID
 		}
 		DB.Save(&quizAnswer)
